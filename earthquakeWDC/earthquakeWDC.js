5ee4a457199bfcbec2fd7fbadf2b98890a9c06fb
Edmund Pizzarello <61759340+epizzarello@users.noreply.github.com>
2022-06-29 11:26:56 -0400

    Update earthquakeWDC.js

--- earthquakeWDC/earthquakeWDC.js
+++ earthquakeWDC/earthquakeWDC.js
@@ -28,7 +28,23 @@
     };
 
     myConnector.getData = function (table, doneCallback) {
+    $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
+        var feat = resp.features,
+            tableData = [];
 
+        // Iterate over the JSON object
+        for (var i = 0, len = feat.length; i < len; i++) {
+            tableData.push({
+                "id": feat[i].id,
+                "mag": feat[i].properties.mag,
+                "title": feat[i].properties.title,
+                "location": feat[i].geometry
+            });
+        }
+
+        table.appendRows(tableData);
+        doneCallback();
+    });
     };
 
     tableau.registerConnector(myConnector);
